d6218872372bc4a4ea0bf96b98496a5219049f49
OMosimege <onalerona.mosimege@gmail.com>
2025-01-19 11:26:32 +0200

    added views

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Employee
+from .serializers import EmployeeSerializer
 
-# Create your views here.
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['first_name', 'last_name', 'email']
